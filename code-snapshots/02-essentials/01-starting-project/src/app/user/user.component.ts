c8d200f7ad116fcb800a7b3f342f9458ce8cbc74
Billy Du Preez <billy.du-preez@partner.co.za>
2026-01-21 07:02:30 +0200

    add event lisener

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -13,4 +13,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
 
+  get avatarUrl() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onSelectUser() {
+    console.log('User selected');
+  }
+
 }
