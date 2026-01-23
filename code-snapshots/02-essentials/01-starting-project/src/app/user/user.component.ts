08cdbe3443ba51cff49568c007cb6fb546507918
Billy Du Preez <billy.du-preez@partner.co.za>
2026-01-23 04:58:04 +0200

    adding inputs and type

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, computed, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true})avatar!: string;
-  @Input({required: true})name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  select = output<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.user.avatar;
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar();
+  // }
 
   onSelectUser() {
-    
+    this.select.emit(this.user.id);
   }
-
 }
