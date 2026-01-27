7630c97591733f84d24976dbd947ac2df7edcd49
Billy Du Preez <billy.du-preez@partner.co.za>
2026-01-27 05:23:22 +0200

    code complete task button

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -39,4 +39,8 @@ export class TasksComponent {
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId)
   }
+
+  onTaskComplete(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId)
+  }
 }
